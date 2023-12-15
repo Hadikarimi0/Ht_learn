7f190ed83f2535491fc4d7c00774244270260168
Hadikarimi0 <114799735+Hadikarimi0@users.noreply.github.com>
2023-12-15 19:12:03 +0330

    Update and rename بات جدید.py to main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import time
 import datetime
 import threading
 from collections import Counter
-bot = Client(session="kkkkkkk")
+bot = Client(session="bot3")
 bot.leave_chat("g0DvrV803b1e605a46e624195e9cf628")
 Addmins = ["u0GHqaC0d9a7e57ba4cd79c18990ed66","u0GHXeO087417f6b19ba05dafe2b26c3"]
 group =["g0Cl26m0351c826a9edd43267d561974"]
